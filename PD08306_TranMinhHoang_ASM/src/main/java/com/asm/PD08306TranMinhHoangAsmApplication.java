84ad6fce58d0033f57588b25d09c7b24442805b5
HoangDaiSA <minhhoangdaigl1@gmail.com>
2024-07-27 05:03:50 +0700

    init project

---
+++ PD08306_TranMinhHoang_ASM/src/main/java/com/asm/PD08306TranMinhHoangAsmApplication.java
@@ -0,0 +1,15 @@
+package com.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class PD08306TranMinhHoangAsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PD08306TranMinhHoangAsmApplication.class, args);
+	}
+
+}
